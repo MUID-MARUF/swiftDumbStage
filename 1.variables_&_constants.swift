c2a86698b1792afd97a319e19fea4550acf21d93
Muid Hasan Maruf <121661366+MUID-MARUF@users.noreply.github.com>
2024-03-27 01:12:42 +0600

    Add files via upload

---
+++ 1.variables_&_constants.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Cocoa
+
+/*
+ There are basically two types of data in SWIFT.
+ 1. Variable (Can manipulate later)
+ 2. Constants (Stiff as Apple)
+*/
+
+var muid = "Beginner Developer"
+print(muid)
+muid = "Bad Developer"
+print(muid)
+
+let sami = "Awesome Programmer"
+print(sami)
+
+/*
+Changing the value of constant is not allowed -->
+sami = "GOAT Programmer"
+*/
+
